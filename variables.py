2d23ddf536fde35c9781de0fe97423751a59dfd4
Dennisco12 <akinwonjowodennisco@gmail.com>
2024-02-29 09:30:15 +0100

    Updated api security and adjusted trade settings

--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 capital = 100
-risk = 0.02 * capital       # 2% of the capital
+risk = 0.05 * capital       # 2% of the capital
 reward = 0.01 * capital     # 1% of the capital
 leverage = 10
 timeframe = '5m'
