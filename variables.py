561f274b9c1ccad6f02f606c65ecf3fe79edde2a
Dennis Akinwonjowo <99836377+Dennisco12@users.noreply.github.com>
2024-03-10 10:43:31 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 capital = 100
-risk = 0.01 * capital       # 3% of the capital
-reward = 0.02 * capital     # 1% of the capital
+risk = 0.03 * capital       # 2% of the capital
+reward = 0.01 * capital     # 1% of the capital
 leverage = 10
 timeframe = '5m'
 confirmation_timeframe = '15m'
