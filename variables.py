686d6df9280cfeba1946ead82ce9a9ec14fec832
Dennisco12 <akinwonjowodennisco@gmail.com>
2024-03-23 07:52:43 +0100

    Enable sl adjustment to breakeven

--- variables.py
+++ variables.py
@@ -2,7 +2,7 @@
 
 capital = 100
 risk = 0.015       # % of the capital
-reward = 0.01     # % of the capital
+reward = 0.02     # % of the capital
 leverage = 5
 timeframe = '5m'
 confirmation_timeframe = '15m'
