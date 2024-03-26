4d86605d4761f4fc2c50e111605f06d363b2d994
Dennisco12 <akinwonjowodennisco@gmail.com>
2024-03-26 14:47:01 +0100

    Adjusted rr

--- variables.py
+++ variables.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 capital = 100
-risk = 0.1       # % of the capital
-reward = 0.0075     # % of the capital
+risk = 0.05       # % of the capital
+reward = 0.01     # % of the capital
 leverage = 10
 timeframe = '5m'
 confirmation_timeframe = '15m'
