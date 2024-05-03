7c2c14b7b351a45e5d4f757febb391135a600d54
Dennis Akinwonjowo <99836377+Dennisco12@users.noreply.github.com>
2024-05-03 10:56:53 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,10 +2,11 @@
 
 capital = 100
 risk = 0.05       # % of the capital
-reward = 0.05     # % of the capital
+reward = 0.005     # % of the capital
 leverage = 10
 timeframe = '5m'
 confirmation_timeframe = '15m'
+safety_factor = 0.2
 
 from exchange import bybit as exchange
 exchange = exchange
