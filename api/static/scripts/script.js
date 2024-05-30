5b3438be4ab3f03cb12582c448efeac45753bd15
Dennisco12 <akinwonjowodennisco@gmail.com>
2024-05-30 07:26:09 +0100

    Adjusted settings

--- api/static/scripts/script.js
+++ api/static/scripts/script.js
@@ -4,7 +4,8 @@ var token = window.localStorage.getItem("session_id")
 
 class Requests {
     constructor() {
-        this.api = "https://scalper-cyk6.onrender.com/bot"
+        // this.api = "https://scalper-cyk6.onrender.com/bot"
+        this.api = "https://scalper-julg.onrender.com/bot"
         // this.api = "http://localhost:8000/bot"
     }
 
