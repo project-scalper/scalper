209351c7c0556c1aeac3285328db5d94226e9bf0
Dennis Akinwonjowo <99836377+Dennisco12@users.noreply.github.com>
2024-07-09 05:55:46 +0100

    Update executor.py

--- executor/executor.py
+++ executor/executor.py
@@ -263,15 +263,18 @@ class Executor(Checker):
             self.enter_trade()
 
             # fetch end balance
-            # for i in range(3):
-            #     end_balance = self.exchange.fetch_balance()['free'].get("USDT", 0)
-            #     if end_balance != 0:
-            #         break
-            #     if i == 2:
-            #         adapter.info(f"{self.symbol} - Unable to fetch end balance for trade")
-            # pnl = end_balance - start_balance
-            if self.pnl != 0:
-                self.update_bot()
+            for i in range(3):
+                end_balance = self.exchange.fetch_balance()['free'].get("USDT", 0)
+                if end_balance != 0:
+                    break
+                if i == 2:
+                    adapter.info(f"{self.symbol} - Unable to fetch end balance for trade")
+            pnl = end_balance - start_balance
+            if not hasattr(self, 'pnl'):
+                self.pnl = pnl
+                
+            # if self.pnl != 0:
+            self.update_bot()
             # if self.bot.today_pnl >= self.daily_target:
             #     setattr(self.bot, "target_reached", True)
             #     setattr(self.bot, "target_date", datetime.now().day)
