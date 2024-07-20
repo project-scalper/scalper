cb311708a7ae00a15bc67f9f0a35f601bfb51b5a
Dennis Akinwonjowo <99836377+Dennisco12@users.noreply.github.com>
2024-07-20 09:58:50 +0100

    Update bot.py

--- api/blueprint/bot.py
+++ api/blueprint/bot.py
@@ -85,8 +85,8 @@ def update_bot():
     data = request.get_json()
     for key, val in data.items():
         if key == 'capital':
-            print("capital - " + type(val) + " : " + val)
-            print("Balance - " + type(bot.balance) + " : " + bot.balance)
+            print("capital - " + str(type(val)) + " : " + val)
+            print("Balance - " + str(type(bot.balance)) + " : " + bot.balance)
             if int(val) < bot.balance:
                 return jsonify('Insufficient balance in wallet'), 400
         setattr(bot, key, val)
